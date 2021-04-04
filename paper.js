8c1dbe8b1421f21bb126d68079c091a0be9e9144
Tanay120 <45686224+Tanay120@users.noreply.github.com>
2021-04-04 21:45:47 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class Paper {
+    constructor( x, y) {
+      var options = {
+          isStatic:false,
+          'restitution':0.3,
+          'friction':0,
+          'density':1.2
+      }
+      this.image = loadImage("paper.png");
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      World.add(world, this.body);
+    }
+    display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    rect(0, 0, this.width, this.height);
+    pop();
+    }
+  }
